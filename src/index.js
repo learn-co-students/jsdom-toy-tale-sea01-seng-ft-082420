0a394391f207f15bb8241b9125c65fdc795be67e
JacksonBey <jacksongrowson@gmail.com>
2020-10-12 20:50:03 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 let addToy = false;
-
+let editToys = false;
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
@@ -50,6 +50,7 @@ function addToys(toys) {
   toys.forEach(toy => {
     let h2 = document.createElement('h2')
     h2.innerHTML = toy.name
+    // h2.contentEditable = "true"
 
     let img = document.createElement("img")
     img.src = toy.image
@@ -75,9 +76,47 @@ function addToys(toys) {
       deleteToy(toy)
     })
 
+    let editbtn = document.createElement('button')
+    editbtn.innerHTML = "edit"
+    editbtn.addEventListener("click", (e) => {
+      let div = document.getElementById(toy.id)
+      console.log(div)
+      div.innerHTML = 
+      `
+      <form class="edit-toy-form"   id = "${toy.id}h">
+      <h3>Edit</h3>
+
+      <input
+        type="text"
+        name="name"
+        value=""
+        placeholder="${toy.name}"
+        class="input-text"
+      />
+      <br />
+      <input
+        type="text"
+        name="image"
+        value=""
+        placeholder="${toy.image}"
+        class="input-text"
+      />
+      <br />
+      <input
+        type="submit"
+        name="submit"
+        value="Submit Edit"
+        class="submit"
+      />
+    </form>
+      `
+      patchToy(toy)
+    })
+
     let div = document.createElement('div')
+    div.id = toy.id
     div.setAttribute('class', 'card')
-    div.append(h2, img, p, likebtn, deletebtn)
+    div.append(h2, img, p, likebtn, deletebtn, editbtn)
 
     let toyContainer = document.querySelector("#toy-collection")
     toyContainer.append(div)
@@ -112,6 +151,26 @@ function postToy(toy) {
   }))
 }
 
+function patchToy(toy) {
+  let toyEdited = document.getElementById(toy.id + "h")
+  toyEdited.addEventListener("submit", function (e){
+      fetch('http://localhost:3000/toys/' + toy.id, {
+        method: "PATCH",
+        headers: {
+            "Content-Type" : "application/json",
+            Accept : "application/json" 
+        },
+        body : JSON.stringify({
+          name : e.target.name.value,
+          image : e.target.image.value,
+          likes: 0
+        }) 
+      })
+      .then(res => res.json())
+      .then(json => console.log(json))
+  })
+}
+
 function deleteToy(toy) {
   fetch('http://localhost:3000/toys/' + toy.id, {
     method: "DELETE"
@@ -123,3 +182,8 @@ function deleteToy(toy) {
     fetchToys()
   })
 }
+
+// function editToy(toy) {
+//   fetch('http://localhost:3000/toys/' + toy.id)
+// }
+
